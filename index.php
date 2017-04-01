4f8a0589d0b9f858d495beb5cc5eaf7cebd4d7c0
nehasinha44 <nehasinha44.jsr@gmail.com>
2017-04-01 09:34:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 session_start();
 ob_start();
 $module_name = "sessionlogin";
-include_once("config.php");
-include_once("controller/Controller.php");
+include_once "config.php";
+include_once "controller/Controller.php";
 $controller = new Controller();
 $controller->index();
 
 ?>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="components/login.css">
-<script type="text/javascript" language="javascript"  src="components/index.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+<script href="components/index.js" type="text/javascript" ></script>
